ddad5476dd4a058bdacffdc4e1acc9b312987992
daniloraf <54066949+daniloraf@users.noreply.github.com>
2021-09-07 17:46:18 -0300

    my first react mobile app

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Meu primeiro Mobile App</Text>
+      <Button title="Já sei construir um mobile app" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
